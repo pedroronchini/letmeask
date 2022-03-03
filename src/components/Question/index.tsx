4dcb385de5c6c301d34e0e319b0a740f93e014f9
Pedro Ronchini <pe-ronchini@hotmail.com>
2022-03-03 16:24:02 -0300

    version 1.3

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,4 +1,4 @@
-
+import { ReactNode } from 'react'
 import './style.scss';
 
 type QuestionProps = {
@@ -6,12 +6,14 @@ type QuestionProps = {
   author: {
     name: string;
     avatar: string;
-  }
+  },
+  children?: ReactNode;
 }
 
 export function Question({
   content,
-  author
+  author,
+  children
 }: QuestionProps) {
   return (
     <div className="question">
@@ -21,7 +23,9 @@ export function Question({
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
-        <div></div>
+        <div>
+          {children}
+        </div>
       </footer>
     </div>
   );
